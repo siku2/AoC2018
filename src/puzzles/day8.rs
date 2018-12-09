1cc8aea9a840f487c24e5764c02437c8c111e673
Simon <simon@siku2.io>
2018-12-09 15:15:43 +0100

    day 8

---
+++ src/puzzles/day8.rs
@@ -0,0 +1,71 @@
+use std::io::BufRead;
+
+use super::utils;
+
+fn count_meta<'a, T: Iterator<Item=&'a String>>(input: &mut T) -> u32 {
+    let mut sum = 0;
+    let child_nodes = input.next().unwrap().parse().unwrap();
+    let meta_nodes = input.next().unwrap().parse().unwrap();
+
+    for _ in 0..child_nodes {
+        sum += count_meta(input);
+    }
+
+    for _ in 0..meta_nodes {
+        let value: u32 = input.next().unwrap().parse().unwrap();
+        sum += value;
+    }
+
+    sum
+}
+
+
+pub fn solve_first<T: BufRead>(input: T) -> u32 {
+    let lines = utils::get_lines(input);
+
+    count_meta(&mut lines.iter())
+}
+
+fn count_meta_referenced<'a, T: Iterator<Item=&'a String>>(input: &mut T) -> u32 {
+    let mut sum = 0;
+    let child_nodes = input.next().unwrap().parse().unwrap();
+    let meta_nodes = input.next().unwrap().parse().unwrap();
+
+    if child_nodes > 0 {
+        let mut sums: Vec<u32> = Vec::new();
+
+        for _ in 0..child_nodes {
+            sums.push(count_meta_referenced(input));
+        }
+
+        for _ in 0..meta_nodes {
+            let index: usize = input.next().unwrap().parse().unwrap();
+            if index == 0 {
+                continue;
+            }
+            sum += sums.get(index - 1).unwrap_or(&0);
+        }
+    } else {
+        for _ in 0..meta_nodes {
+            let value: u32 = input.next().unwrap().parse().unwrap();
+            sum += value;
+        }
+    }
+
+    sum
+}
+
+
+pub fn solve_second<T: BufRead>(input: T) -> u32 {
+    let lines = utils::get_lines(input);
+
+    count_meta_referenced(&mut lines.iter())
+}
+
+pub fn solve<T: BufRead>(problem: u8, input: T) -> Result<String, String> {
+    match problem {
+        1 => Result::Ok(solve_first(input).to_string()),
+        2 => Result::Ok(solve_second(input).to_string()),
+        _ => Result::Err("This problem only has 2 parts!".to_string())
+    }
+}
--- src/puzzles/mod.rs
+++ src/puzzles/mod.rs
@@ -7,6 +7,7 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 
 mod utils;
 
@@ -19,6 +20,7 @@ pub fn solve<T>(day: u8, part: u8, input: T) -> Result<String, String> where T: 
         5 => day5::solve(part, input),
         6 => day6::solve(part, input),
         7 => day7::solve(part, input),
+        8 => day8::solve(part, input),
         _ => Result::Err(format!("Can't handle day {} yet", day))
     }
 }
